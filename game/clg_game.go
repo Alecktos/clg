7b972617884fc2497327a1b116dad659870dba98
Alecktos <alexander.berlind@protonmail.com>
2024-10-23 14:17:49 +0200

    Extract game to own module

---
+++ game/clg_game.go
@@ -0,0 +1,29 @@
+package game
+
+import (
+	"github.com/Alecktos/couplesLoveGame/view"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+var (
+	clgSprite *view.ClgSprite = view.NewClgSprite()
+)
+
+type ClgGame struct{}
+
+func (g *ClgGame) Update() error {
+	return nil
+}
+
+func (g *ClgGame) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello, World!")
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(50, 50)
+	op.GeoM.Scale(0.5, 0.5)
+	screen.DrawImage(clgSprite.Img, op)
+}
+
+func (g *ClgGame) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
--- main.go
+++ main.go
@@ -1,38 +1,15 @@
 package main
 
 import (
-	"github.com/Alecktos/couplesLoveGame/view"
+	"github.com/Alecktos/couplesLoveGame/game"
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"log"
 )
-
-var (
-	clgSprite *view.ClgSprite = view.NewClgSprite()
-)
-
-type Game struct{}
-
-func (g *Game) Update() error {
-	return nil
-}
-
-func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(50, 50)
-	op.GeoM.Scale(0.5, 0.5)
-	screen.DrawImage(clgSprite.Img, op)
-}
-
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
-}
 
 func main() {
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	if err := ebiten.RunGame(&game.ClgGame{}); err != nil {
 		log.Fatal(err)
 	}
 }
