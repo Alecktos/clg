c867f1c3ecc4b363e4944f8a41dfe4daa6fb5429
Alecktos <alexander.berlind@protonmail.com>
2024-10-24 10:44:46 +0200

    Test drawing sprite

--- view/clg_sprite.go
+++ view/clg_sprite.go
@@ -2,25 +2,26 @@ package view
 
 import (
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"log"
 )
 
 type ClgSprite struct {
 	Img *ebiten.Image
 }
 
-/*
-	func (c *ClgSprite) loadImage() {
-		var err error
-		var image *ebiten.Image
-		image, _, err = ebitenutil.NewImageFromFile("brick.png")
-		c.Img = image
-		if err != nil {
-			log.Fatal(err)
-		}
+func (c *ClgSprite) loadImage() {
+	var err error
+	var image *ebiten.Image
+	image, _, err = ebitenutil.NewImageFromFile("brick.png")
+	c.Img = image
+	if err != nil {
+		log.Fatal(err)
 	}
-*/
+}
+
 func NewClgSprite() *ClgSprite {
 	clgImage := &ClgSprite{}
-	//clgImage.loadImage()
+	clgImage.loadImage()
 	return clgImage
 }
