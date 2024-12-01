087f68b4c4308bc56d39534b2f8e1a7164c1b60a
Alecktos <alexander.berlind@protonmail.com>
2024-12-01 21:28:44 +0100

    Always draw image

--- game/game_brick.go
+++ game/game_brick.go
@@ -3,7 +3,6 @@ package game
 import (
 	"github.com/Alecktos/clg/assets/images"
 	"github.com/Alecktos/clg/game/common"
-	"github.com/Alecktos/clg/game/input"
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
@@ -17,16 +16,16 @@ func (c *ClgSprite) Update() {
 }
 
 func (c *ClgSprite) Draw(screen *ebiten.Image) {
-	if input.IsPressed() && c.Contains(input.Position()) {
-		op := &ebiten.DrawImageOptions{}
-		imgSourceSize := c.Img.Bounds().Size()
-		scaleX := float64(c.Width) / float64(imgSourceSize.X)
-		scaleY := float64(c.Height) / float64(imgSourceSize.Y)
-		op.GeoM.Scale(scaleX, scaleY)
-		op.GeoM.Translate(float64(c.Position.X), float64(c.Position.Y))
+	//if input.IsPressed() && c.Contains(input.Position()) { does not work on phone
+	op := &ebiten.DrawImageOptions{}
+	imgSourceSize := c.Img.Bounds().Size()
+	scaleX := float64(c.Width) / float64(imgSourceSize.X)
+	scaleY := float64(c.Height) / float64(imgSourceSize.Y)
+	op.GeoM.Scale(scaleX, scaleY)
+	op.GeoM.Translate(float64(c.Position.X), float64(c.Position.Y))
 
-		screen.DrawImage(c.Img, op)
-	}
+	screen.DrawImage(c.Img, op)
+	//}
 }
 
 func (c *ClgSprite) loadImage() error {
